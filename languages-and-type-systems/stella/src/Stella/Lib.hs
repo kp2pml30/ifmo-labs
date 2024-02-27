1e730f659ab3fa2b43651d816db13525088e7641
kp2pml30 <kp2pml30@gmail.com>
2024-02-27 15:32:58 +0300

    prepare for solution

--- languages-and-type-systems/stella/src/Stella/Lib.hs
+++ languages-and-type-systems/stella/src/Stella/Lib.hs
@@ -8,17 +8,22 @@ import Control.Monad
 import Control.Monad.Except
 import Control.Monad.State
 
-import Gen.PrintSyntax ( Print, printTree )
+import qualified Gen.PrintSyntax as Printer -- ( Print, printTree )
 
 import Data.Either
+import qualified Data.DisjointSet as DisjointSet
+import Data.DisjointSet (DisjointSet)
 
 import Stella.Errs
 import Gen.AbsSyntax
 
 data CheckerState
 	= CheckerState
-	{
+	{ typeVars :: DisjointSet Int
 	}
+
+emptyCheckerState :: CheckerState
+emptyCheckerState = CheckerState DisjointSet.empty
 
 data ErrorData
 	= ErrorData
@@ -30,9 +35,12 @@ newtype CheckerMonad a
   = CheckerMonad { unCheckerMonad :: StateT CheckerState (Except ErrorData) a }
   deriving newtype (Functor, Applicative, Monad, MonadError ErrorData, MonadState CheckerState)
 
+todo :: CheckerMonad a
+todo = throwError $ ErrorData ERROR_TODO []
+
 doCheck :: Program -> CheckerMonad ()
 doCheck _ = do
-	pure ()
+	todo
 
 check :: Program -> [ErrorData]
-check p = either (:[]) (const []) $ runExcept $ evalStateT (unCheckerMonad $ doCheck p) (CheckerState)
+check p = either (:[]) (const []) $ runExcept $ evalStateT (unCheckerMonad $ doCheck p) emptyCheckerState
--- languages-and-type-systems/stella/test/run-golden.rb
+++ languages-and-type-systems/stella/test/run-golden.rb
@@ -9,98 +9,98 @@ $errs = 0
 $tests = 0
 
 class ThreadPool
-    def initialize
-        @max = Etc::nprocessors
-        @threads = []
-    end
+	def initialize
+		@max = Etc::nprocessors
+		@threads = []
+	end
 
-    def wait
-        while @threads.size > 0
-            filter
-        end
-    end
+	def wait
+		while @threads.size > 0
+			filter
+		end
+	end
 
-    def filter
-        sleep 0.05
-        @threads.filter! { |t| t.alive? }
-    end
+	def filter
+		sleep 0.05
+		@threads.filter! { |t| t.alive? }
+	end
 
-    def run
-        while @threads.size >= @max
-            filter
-        end
-        @threads << Thread.new {
-            yield
-        }
-    end
+	def run
+		while @threads.size >= @max
+			filter
+		end
+		@threads << Thread.new {
+			yield
+		}
+	end
 end
 
 def run_get_err(path, expected_error)
-    real_path = File.expand_path path
-    stdout_str, stderr_str, status = Open3.capture3(
-        'stack', '--silent', 'exec',
-        '--cwd=..',
-        'stella-exe', real_path
-    )
-    got_err = '<unknown>'
-    if stderr_str =~ /\bERROR_([a-zA-Z0-9_]+)\b/
-        got_err = /\bERROR_([a-zA-Z0-9_]+)\b/.match(stderr_str)[0]
-    end
-    put_outs = Proc.new {
-        if status != 0
-            puts "\tdetected error is #{got_err}"
-        end
-        if stdout_str != ""
-            puts stdout_str.gsub(/^/, "\t")
-        end
-        if stderr_str != ""
-            puts stderr_str.gsub(/^/, "\t")
-        end
-    }
-    if expected_error.nil?
-        if status != 0
-            $errs += 1
-            puts "✗ #{path} expected to succeed"
-            put_outs.()
-            return
-        end
-    else
-        if status == 0
-            $errs += 1
-            puts "✗ #{path} expected to fail with #{expected_error}"
-            put_outs.()
-            return
-        end
-        if got_err != expected_error
-            $errs += 1
-            puts "✗ #{path} expected to fail with #{expected_error}, failed with #{got_err}"
-            put_outs.()
-            return
-        end
-    end
-    puts "✓ #{path}"
+	real_path = File.expand_path path
+	stdout_str, stderr_str, status = Open3.capture3(
+		'stack', '--silent', 'exec',
+		'--cwd=..',
+		'stella-exe', real_path
+	)
+	got_err = '<unknown>'
+	if stderr_str =~ /\bERROR_([a-zA-Z0-9_]+)\b/
+		got_err = /\bERROR_([a-zA-Z0-9_]+)\b/.match(stderr_str)[0]
+	end
+	put_outs = Proc.new {
+		if status != 0
+			puts "\tdetected error is #{got_err}"
+		end
+		if stdout_str != ""
+			puts stdout_str.gsub(/^/, "\t")
+		end
+		if stderr_str != ""
+			puts stderr_str.gsub(/^/, "\t")
+		end
+	}
+	if expected_error.nil?
+		if status != 0
+			$errs += 1
+			puts "✗ #{path} expected to succeed"
+			put_outs.()
+			return
+		end
+	else
+		if status == 0
+			$errs += 1
+			puts "✗ #{path} expected to fail with #{expected_error}"
+			put_outs.()
+			return
+		end
+		if got_err != expected_error
+			$errs += 1
+			puts "✗ #{path} expected to fail with #{expected_error}, failed with #{got_err}"
+			put_outs.()
+			return
+		end
+	end
+	puts "✓ #{path}"
 end
 
 tp = ThreadPool.new
 
 Dir::glob("./golden/ok/**/*.st") { |f|
-    $tests += 1
-    tp.run {
-        run_get_err f, nil
-    }
+	$tests += 1
+	tp.run {
+		run_get_err f, nil
+	}
 }
 
 Dir::glob("./golden/bad/**/*.st") { |f|
-    $tests += 1
-    m = /\/bad\/(?<err>[^\/]+)\//.match(f)
-    tp.run {
-        run_get_err f, m['err']
-    }
+	$tests += 1
+	m = /\/bad\/(?<err>[^\/]+)\//.match(f)
+	tp.run {
+		run_get_err f, m['err']
+	}
 }
 
 tp.wait
 
 puts "Failed #{$errs}/#{$tests}"
 if $errs != 0
-    exit false
+	exit false
 end
